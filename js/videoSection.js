67a2085817ff664837795f61f27dc14e8b233dd6
tusharpramanikbd <tusharpramanikbd@gmail.com>
2021-10-25 12:57:42 +0600

    video data fetch from api

--- js/videoSection.js
+++ js/videoSection.js
@@ -10,30 +10,38 @@ await fetchJson()
 function setVideoData(videoList) {
   const videoListHtml = videoList
     .map((video) => {
+      const {
+        name,
+        image: img,
+        duration,
+        title,
+        avater,
+        views,
+        created,
+      } = video;
       return `<div class="div-video-item">
           <div class="section-video-img-div">
             <img
-              src="./images/project-1.jpeg"
+              src="${img}"
               class="section-video-img"
-              alt=""
+              alt="${name}"
             />
-            <h5>5:00</h5>
+            <h5>${duration}</h5>
           </div>
           <div class="section-video-info-div">
             <div class="section-video-info-div-top">
               <img
-                src="./images/hero-img-small.jpeg"
+                src="${avater}"
                 class="section-video-info-avater"
                 alt=""
               />
-              <h1>javascript progress bar animation tutorial</h1>
+              <h1>${title}</h1>
             </div>
             <div class="section-video-info-div-middle">
-              <p>dev ed</p>
+              <p>${name}</p>
               <i class="fas fa-check-circle"></i>
             </div>
-            <p class="section-video-info-div-bottom-title">
-              7.1<span>K</span> views &middot; 11 hours ago
+            <p class="section-video-info-div-bottom-title">${views} views &middot; ${created}
             </p>
           </div>
         </div>`;
