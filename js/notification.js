a04c63d98b05ddd03a190504850fb041afab62d8
tusharpramanikbd <tusharpramanikbd@gmail.com>
2021-12-01 23:55:02 +0600

    added jsdoc to notification.js file

--- js/notification.js
+++ js/notification.js
@@ -1,15 +1,20 @@
 import { 
     notification,
     notificationImg,
-    notificationText } from "./staticVariables/svNotification.js";
+    notificationText 
+} from "./staticVariables/svNotification.js";
 
-// Showing offilne notification
+/**
+ * This function show offline notification by adding "show-notification" class
+ */
 function showOfflineNotification(){
     notification.classList.add("show-notification");
 }
 
-// Changing content of notification to online
-// After some time hiding the motification
+/**
+ * Change content of notification to online
+ * After some time hide the notification
+ */
 function hideOfflineNotification(){
     notification.classList.add("notification-online");
     notificationImg.classList.add("notification-img-online");
